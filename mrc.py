57f4c729c5c4e1283a32dcc9380ab32ce32a81ba
lim4349 <rockmanzero@naver.com>
2023-01-02 08:39:25 +0000

    Disable Arrow cache

--- mrc.py
+++ mrc.py
@@ -9,6 +9,8 @@ from transformers import AutoModelForQuestionAnswering, AutoTokenizer, DataColla
 
 from utils.trainer_qa import QuestionAnsweringTrainer
 from utils.utils_qa import check_sanity, postprocess_qa_predictions
+from datasets import disable_caching
+disable_caching()
 
 logger = logging.getLogger(__name__)
 
